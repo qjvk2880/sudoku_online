2652f80d10b4b45223aedc9d9e802fdada673d50
Kim Min Uk <qjvk2880@gmail.com>
2022-03-31 02:50:41 +0900

    db에서 타임데이터 가져와서 html에 띄우기 성공
    데이터 갯수 10개 안넘으면 에러나서 예외처리완료

--- public/db_functions.js
+++ public/db_functions.js
@@ -1,11 +1,11 @@
-var db = require("./db");
+var db = require("../db");
 
 exports.add_record = function (hrs, min, sec, userName) {
   var time =
     (hrs > 9 ? hrs : "0" + hrs) +
     (min > 9 ? min : "0" + min) +
     (sec > 9 ? sec : "0" + sec);
-  console.log(time);
+  
   db.query(
     `INSERT INTO time_record(name,time)
   VALUES(?,?)`,
@@ -14,4 +14,16 @@ exports.add_record = function (hrs, min, sec, userName) {
       if (error) throw error;
     }
   );
+};
+
+exports.get_ranking = function (callback) {
+  
+  db.query(`SELECT * FROM time_record ORDER BY TIME ASC LIMIT 10`, function (error, ranking) {
+    if (error){
+      throw error;
+    }   
+    
+      return callback(ranking);
+  });
+  
 };
--- server.js
+++ server.js
@@ -2,33 +2,60 @@ var http = require("http");
 var url = require("url");
 var qs = require("querystring");
 var fs = require("fs");
-var path = require('path');
-var express = require('express');
-var functions = require('./sudoku_functions'); 
-var dbFunctions = require('./db_functions'); 
+var path = require("path");
+var express = require("express");
+var functions = require("./sudoku_functions");
+var dbFunctions = require("./public/db_functions");
+const db = require("./db");
+const { send } = require("express/lib/response");
 const app = express();
 
-app.use('/public', express.static(__dirname + '/public'));
+app.use("/public", express.static(__dirname + "/public"));
 
-app.listen(3000, function () {
-});
+app.listen(3000, function () {});
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + '/sudoku.html');
+  res.sendFile(__dirname + "/sudoku.html");
 });
 
 app.get("/result", (req, res) => {
-    console.log(req.query);
+  dbFunctions.add_record(
+    req.query.hrs,
+    req.query.min,
+    req.query.sec,
+    req.query.name
+    );
+    dbFunctions.get_ranking(function (ranking) {
+        var listHTML = "";
+        var length = (ranking.length < 10) ?  ranking.length : 10 ;
+        for (var i = 0; i < length; i++){
+            listHTML+=`<li>${ranking[i].name} : ${ranking[i].time}</li>`
+        }
+        res.send(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <title>solved</title>
+
+        </head>
+        <body>
+            <h1>문제 해결 성공!</h1>
+            <ol class="ranking">
+                ${listHTML}
+            </ol>
+
+
+
+        </body>
+        </html>
+        `);
+    });
     
-    if (functions.check_submit(req.query) == true) {
-        //대충 db에 올리는 함수 매개변수 req.query hrs min sec name
-        dbFunctions.add_record(req.query.hrs, req.query.min, req.query.sec, req.query.name);
-        res.sendFile(__dirname + '/solved.html');
-        
-    } else {
-        dbFunctions.add_record(req.query.hrs, req.query.min, req.query.sec, req.query.name);
-        res.sendFile(__dirname + '/result2.html');
-    };
-    
-    
+  // if (functions.check_submit(req.query) == true) {
+  //     //대충 db에 올리는 함수 매개변수 req.query hrs min sec name
+
+  // } else {
+
+  //     res.sendFile(__dirname + '/failed.html');
+  // };
 });
